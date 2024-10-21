7078d232dc305f9a8d2047ebfc5da6de69072923
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-21 18:34:11 -0300

    refatorando cadastro e login

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,11 +1,24 @@
 describe('Cadastro', () => {
-  it('Cadastro com sucesso', () => {
+  beforeEach(() => { //Representa uma function 'Antes de cada'. Para rodar determinados comandos antes da execução do caso de teste
     cy.visit('https://adopet-frontend-cypress.vercel.app/'); //visita uma página web
     cy.get('[data-test="register-button"]').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
+  })
+
+
+  it('Cadastro com sucesso', () => {
     cy.get('[data-test="input-name"]').type('Alan Cardoso'); //pega o elemento do campo 'Nome' na tela e faz o input do texto
     cy.get('[data-test="input-email"]').type('alcardosos@hotmail.com'); //pega o elemento do campo 'Email' na tela e faz o input do texto
     cy.get('[data-test="input-password"]').type('Teste123'); //pega o elemento do campo 'Senha' na tela e faz o input do texto
     cy.get('[data-test="input-confirm-password"]').type('Teste123'); //pega o elemento do campo 'Confirme sua senha' na tela e faz o input do texto
     cy.get('[data-test="submit-button"]').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
+  })
+
+  it('Falha na tentativa de cadastro com os campos em branco', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains('É necessário informar um endereço de email').should('be.visible'); //Comando que verifica se a mensagem está visível na tela
+    cy.contains('Crie uma senha').should('be.visible');
+    cy.contains('Repita a senha criada acima').should('be.visible');
   })
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,4 +9,12 @@ describe('Login', () => {
     cy.get('[data-test="input-loginPassword"]').type('Teste123');
     cy.get('[data-test="submit-button"]').click();
   })
+
+  it('Login com credenciais fora dos padrões', () => {
+    cy.get('[data-test="input-loginEmail"]').type('alcardososhotmail.com');
+    cy.get('[data-test="input-loginPassword"]').type('123');
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains('Por favor, verifique o email digitado').should('be.visible');
+    cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible');
+  })
 })
