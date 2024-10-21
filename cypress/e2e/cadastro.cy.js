b648094cc67ed2e9313defb934ba4f3374069667
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-21 15:42:13 -0300

    refatorando o cadastro e implementando o .gitignore

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,11 +1,11 @@
 describe('Cadastro', () => {
-  it('Cadastro com Sucesso', () => {
+  it('Cadastro com sucesso', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/'); //visita uma página web
-    cy.contains('a', 'Cadastrar').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
-    cy.get('input[name="nome"]').type('Alan Cardoso'); //pega o elemento do campo 'Nome' na tela e faz o input do texto
-    cy.get('input[name="email"]').type('alcardosos@hotmail.com'); //pega o elemento do campo 'Email' na tela e faz o input do texto
-    cy.get('input[name="password"]').type('Teste123'); //pega o elemento do campo 'Senha' na tela e faz o input do texto
-    cy.get('input[name="confirm_password"]').type('Teste123'); //pega o elemento do campo 'Confirme sua senha' na tela e faz o input do texto
-    cy.contains('button', 'Cadastrar').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
+    cy.get('[data-test="register-button"]').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
+    cy.get('[data-test="input-name"]').type('Alan Cardoso'); //pega o elemento do campo 'Nome' na tela e faz o input do texto
+    cy.get('[data-test="input-email"]').type('alcardosos@hotmail.com'); //pega o elemento do campo 'Email' na tela e faz o input do texto
+    cy.get('[data-test="input-password"]').type('Teste123'); //pega o elemento do campo 'Senha' na tela e faz o input do texto
+    cy.get('[data-test="input-confirm-password"]').type('Teste123'); //pega o elemento do campo 'Confirme sua senha' na tela e faz o input do texto
+    cy.get('[data-test="submit-button"]').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
   })
 })
