e0baa44c7046b554b87efb731812927fd2d8ca45
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-22 21:00:53 -0300

    refatorando pets-disponiveis-adocao

--- cypress/e2e/pets-disponiveis-adocao.cy.js
+++ cypress/e2e/pets-disponiveis-adocao.cy.js
@@ -1,7 +1,7 @@
 describe('Pets disponíveis para adoção', () => {
-    
+
     it('Listagem de pets', () => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
-        cy.contains('a', 'Ver pets disponíveis para adoção').click();
+        cy.get('.button').click();
     })
   })
