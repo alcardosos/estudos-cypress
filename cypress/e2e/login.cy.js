254b2b3ac9a8afcb0e4adbb921d02893af0811d6
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-24 19:26:41 -0300

    incluindo comando personalizado para login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,40 +6,30 @@ describe('Login', () => {
 
   it('Login com credenciais válidas', () => {
     cy.get('[data-test="login-button"]').click();
-    cy.get('[data-test="input-loginEmail"]').type('alcardosos@hotmail.com');
-    cy.get('[data-test="input-loginPassword"]').type('Teste123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('alcardosos@hotmail.com', 'Teste123');
   })
 
   it('Login com credenciais fora dos padrões', () => {
     cy.get('[data-test="login-button"]').click();
-    cy.get('[data-test="input-loginEmail"]').type('alcardososhotmail.com');
-    cy.get('[data-test="input-loginPassword"]').type('123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('alcardososhotmail.com', '123');
     cy.contains('Por favor, verifique o email digitado').should('be.visible');
     cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible');
   })
 
   it('Login com credenciais válidas via tela de mensagens', () => {
     cy.get('.header__message').click();
-    cy.get('[data-test="input-loginEmail"]').type('alcardosos@hotmail.com');
-    cy.get('[data-test="input-loginPassword"]').type('Teste123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('alcardosos@hotmail.com', 'Teste123');
   })
 
   it('Login com e-mail não cadastrado', () => {
     cy.get('[data-test="login-button"]').click();
-    cy.get('[data-test="input-loginEmail"]').type('alcardosos@errado.com');
-    cy.get('[data-test="input-loginPassword"]').type('Teste123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('alcardosos@errado.com', 'Teste123');
     cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
   })
 
   it('Login com senha não cadastrada', () => {
     cy.get('[data-test="login-button"]').click();
-    cy.get('[data-test="input-loginEmail"]').type('alcardosos@hotmail.com.com');
-    cy.get('[data-test="input-loginPassword"]').type('Senha123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('alcardosos@hotmail.com', 'Senha123');
     cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
   })
 })
