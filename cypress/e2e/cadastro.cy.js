ff654695041c169c0c3ab403af133edd999c30af
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-24 19:51:57 -0300

    criando comandos personalizados para cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -7,16 +7,10 @@ describe('Cadastro', () => {
 
 
   it('Cadastro com sucesso', () => {
-    cy.get('[data-test="input-name"]').type('Alan Cardoso'); //pega o elemento do campo 'Nome' na tela e faz o input do texto
-    cy.get('[data-test="input-email"]').type('alcardosos@hotmail.com'); //pega o elemento do campo 'Email' na tela e faz o input do texto
-    cy.get('[data-test="input-password"]').type('Teste123'); //pega o elemento do campo 'Senha' na tela e faz o input do texto
-    cy.get('[data-test="input-confirm-password"]').type('Teste123'); //pega o elemento do campo 'Confirme sua senha' na tela e faz o input do texto
-    cy.get('[data-test="submit-button"]').click(); //procura o elemento do botão 'Cadastrar' na tela e efetua o clique
+    cy.cadastro('Alan Cardoso', 'alcardosos@hotmail.com', 'Teste123', 'Teste123');
   })
 
-  it('Falha na tentativa de cadastro com os campos em branco', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.get('[data-test="register-button"]').click();
+  it('Tentativa de cadastro com os campos em branco', () => {
     cy.get('[data-test="submit-button"]').click();
     cy.contains('É necessário informar um endereço de email').should('be.visible'); //Comando que verifica se a mensagem está visível na tela
     cy.contains('Crie uma senha').should('be.visible');
@@ -31,20 +25,12 @@ describe('Cadastro', () => {
   })
 
   it('Cadastro com senhas divergêntes', () => {
-    cy.get('[data-test="input-name"]').type('Alan Almeida'); 
-    cy.get('[data-test="input-email"]').type('alcardosos@gmail.com'); 
-    cy.get('[data-test="input-password"]').type('Teste123'); 
-    cy.get('[data-test="input-confirm-password"]').type('Senha123'); 
-    cy.get('[data-test="submit-button"]').click();
+    cy.cadastro('Alan Almeida', 'alcardosos@gmail.com', 'Teste123', 'Senha123');
     cy.contains('As senhas não batem').should('be.visible');
   })
 
-  it('Cadastro com senhas divergêntes', () => {
-    cy.get('[data-test="input-name"]').type('Alan'); 
-    cy.get('[data-test="input-email"]').type('alcardosos@hotmail.com'); 
-    cy.get('[data-test="input-password"]').type('Senha123'); 
-    cy.get('[data-test="input-confirm-password"]').type('Senha123'); 
-    cy.get('[data-test="submit-button"]').click();
+  it('Cadastro com e-mail já cadastrado', () => {
+    cy.cadastro('Alan', 'alcardosos@hotmail.com', 'Senha123', 'Senha123');
     cy.contains('Falha ao cadastrar!').should('be.visible');
   })
 })
