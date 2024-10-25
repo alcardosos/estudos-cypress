5a66a1102ef0dbef427c9f3685972d1a6b0ac205
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-25 17:45:57 -0300

    implementando validação de nome do usuário no perfil via API

--- cypress/e2e/api-mensagen.cy.js
+++ cypress/e2e/api-mensagen.cy.js
@@ -12,4 +12,18 @@ describe('API Adopet', () => {
             expect(res.body).to.have.property('msg')
         })
     })
+
+    it.only('Nome do perfil', () => {
+        cy.request({
+            method: 'GET' ,
+            url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/cf6ccec9-9c4b-43a9-9813-5fa18e338d65',
+            headers: { authorization }
+           
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('perfil')
+            expect(res.body.perfil.nome).to.be.equal('Alan Cardoso')
+        })
+    })
 })
