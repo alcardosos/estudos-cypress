3407b366e7000533abe9a21ee9d12c5f8f790c32
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-28 08:50:49 -0300

    atualizando o token authorization

--- cypress/e2e/api-mensagen.cy.js
+++ cypress/e2e/api-mensagen.cy.js
@@ -1,5 +1,5 @@
 describe('API Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjZjZjY2VjOS05YzRiLTQzYTktOTgxMy01ZmExOGUzMzhkNjUiLCJhZG9wdGVyTmFtZSI6IkFsYW4gQ2FyZG9zbyIsImlhdCI6MTcyOTg4NDYxNSwiZXhwIjoxNzMwMTQzODE1fQ.jvUkTdkS6vX2Mv_1dYDsDbV-zvK3zeGHDUBIWVNLr-g`
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjZjZjY2VjOS05YzRiLTQzYTktOTgxMy01ZmExOGUzMzhkNjUiLCJhZG9wdGVyTmFtZSI6IkFsYW4gQ2FyZG9zbyIsImlhdCI6MTczMDExNTYxNiwiZXhwIjoxNzMwMzc0ODE2fQ.Cb83M6mhlEo6oAcP_uMArNYrt9gYqvFoelRtROxLgFs`
 
     it('Mensagem da API', () => {
         cy.request({
@@ -9,11 +9,11 @@ describe('API Adopet', () => {
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
-            expect(res.body).to.have.property('msg')
+            expect(res.body).to.have.property('msg')      
         })
     })
 
-    it.only('Nome do perfil', () => {
+    it('Nome do perfil', () => {
         cy.request({
             method: 'GET' ,
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/cf6ccec9-9c4b-43a9-9813-5fa18e338d65',
