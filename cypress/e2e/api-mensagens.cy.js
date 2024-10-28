2cd3f5b9bb2e591f28e0eaad4590deebea97eaa3
Alan Cardoso de Almeida <alcadosos0@gmail.com>
2024-10-28 09:46:43 -0300

    implementando arquivo .env e corrigindo nome do arquivo api-mensagens.cy.js

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,10 @@
 describe('API Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjZjZjY2VjOS05YzRiLTQzYTktOTgxMy01ZmExOGUzMzhkNjUiLCJhZG9wdGVyTmFtZSI6IkFsYW4gQ2FyZG9zbyIsImlhdCI6MTczMDExNTYxNiwiZXhwIjoxNzMwMzc0ODE2fQ.Cb83M6mhlEo6oAcP_uMArNYrt9gYqvFoelRtROxLgFs`
 
     it('Mensagem da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/cf6ccec9-9c4b-43a9-9813-5fa18e338d65',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
@@ -17,7 +16,7 @@ describe('API Adopet', () => {
         cy.request({
             method: 'GET' ,
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/cf6ccec9-9c4b-43a9-9813-5fa18e338d65',
-            headers: { authorization }
+            headers: Cypress.env()
            
         }).then((res) => {
             expect(res.status).to.be.equal(200)
